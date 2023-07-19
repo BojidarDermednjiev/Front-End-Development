d9b1c2f8676a42e8d25f0d101c74b7dc4dc70b32
Bojidar Dermendjiev <116583072+BojidarDermednjiev@users.noreply.github.com>
2023-07-19 14:38:47 +0300

    Delete JavaScript/02. JS Advanced/07. DOM Introduction - Lab directory

--- JavaScript/02. JS Advanced/07. DOM Introduction - Lab/02.Collect List Items/CollectListItems.js
+++
@@ -1,5 +0,0 @@
-function extractText() {
-  let itemNodes = Array.from(document.getElementsByTagName(`li`));
-  let content = itemNodes.map((x) => x.textContent).join(`\n`);
-  document.getElementById(`result`).textContent = content;
-}
